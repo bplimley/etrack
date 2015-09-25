f7e34051d0e412b47c87bab6f769af2b0ff63bd2
Brian Plimley <brian.plimley@gmail.com>
2015-09-25 13:51:49 -0700

    test_from_file.py: now for 2.5um tracks
    from data in algorithms/results/2013sep binned

--- reconstruction/test_from_file.py
+++ reconstruction/test_from_file.py
@@ -8,8 +8,10 @@ import os
 
 import hybridtrack
 
-loadpath = '/mnt/data/MATLAB/data/Electron Track/geant4/'
-loadfile = 'Mat_CCD_SPEC_500k_49_TRK.h5'
+# loadpath = '/mnt/data/MATLAB/data/Electron Track/geant4/'
+# loadfile = 'Mat_CCD_SPEC_500k_49_TRK.h5'
+loadpath = '/mnt/data/MATLAB/data/Electron Track/algorithms/results/2013sep binned'
+loadfile = 'MultiAngle_HT_100_1.h5'
 
 # widgets = [pbar.Percentage(), ' ', pbar.Bar(), ' ', pbar.ETA()]
 # pb = pbar.ProgressBar(widgets=widgets, maxval=1000)
@@ -27,26 +29,34 @@ pbar = progressbar.ProgressBar(
     progressbar.ETA()], maxval = len(h5file))
 pbar.start()
 
+info = [[] for __ in range(len(h5file))]
+alpha = np.zeros(len(h5file))
+
 for i in range(len(h5file)):
     eventname = 'event{:05d}'.format(i)
     if eventname in h5file:
         event = h5file[eventname]
-        image_index = 1
-        for j in range(len(event)-2):
-            trackname = 'trackimage{:02d}'.format(j+1)
-            # print(trackname)
-            # print(event)
-            if trackname in event:
-                this_image_object = event[trackname]
-                this_image = np.zeros(this_image_object.shape)
-                this_image_object.read_direct(this_image)
-                try:
-                    # reverse the transpose MATLAB did during h5create
-                    out = hybridtrack.reconstruct(this_image.T)
-                except hybridtrack.InfiniteLoop:
-                    print('Infinite loop on event #{}, track #{}'.format(i,j))
-                except hybridtrack.NoEndsFound:
-                    print('No ends found on event #{}, track #{}'.format(i,j))
+        trackname = 'trackimage2.5'
+        # print(trackname)
+        # print(event)
+        if trackname in event:
+            this_image_object = event[trackname]
+            this_image = np.zeros(this_image_object.shape)
+            this_image_object.read_direct(this_image)
+            try:
+                # reverse the transpose MATLAB did during h5create
+                __, info[i] = hybridtrack.reconstruct(this_image.T,
+                    pixel_size_um=2.5)
+                alpha[i] = info[i].alpha_deg
+            except hybridtrack.InfiniteLoop:
+                print('Infinite loop on event #{}, track #{}'.format(i,j))
+            except hybridtrack.NoEndsFound:
+                print('No ends found on event #{}, track #{}'.format(i,j))
+            except err:
+                print('Other error:')
+                print(err)
 
     pbar.update(i)
 pbar.finish()
+
+np.save('alpha2.5.npy',alpha)
