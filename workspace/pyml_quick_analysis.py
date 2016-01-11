7c8d39ae547872d3c1b2d2b1187546b342bb7ba3
Brian Plimley <brian.plimley@gmail.com>
2016-01-11 14:11:25 -0800

    pyml_quick_analysis.py: now just doing file-by-file

--- workspace/pyml_quick_analysis.py
+++ workspace/pyml_quick_analysis.py
@@ -24,16 +24,20 @@ def main():
     pixlist = ['10_5', '2_5']
     alglist = ['matlab HT v1.5', 'python HT v1.5', 'python HT v1.5a']
 
-    results_by_file = {}
-    all_results = {}
+    # results_by_file = {}
+    # all_results = {}
     subgroup_name = {}
+    save_group_name = {}
     for pix in pixlist:
-        results_by_file[pix] = {}
-        all_results[pix] = {}
+        # results_by_file[pix] = {}
+        # all_results[pix] = {}
         subgroup_name[pix] = 'pix' + pix + 'noise0'
+        save_group_name[pix] = {}
         for alg in alglist:
-            results_by_file[pix][alg] = []      # to be appended to
-            all_results[pix][alg] = []          # to be replaced with an AR obj
+            # results_by_file[pix][alg] = []      # to be appended to
+            save_group_name[pix][alg] = alg + ', ' + pix
+            # all_results[pix][alg] = []        # to be replaced with an AR obj
+    res = []
 
     pbar = progressbar.ProgressBar(
         widgets=[progressbar.Percentage(), ' ', progressbar.Bar(), ' ',
@@ -41,21 +45,33 @@ def main():
     pbar.start()
 
     for i, f in enumerate(flist):
-        with h5py.File(f, 'r') as h5f:
+        savename = make_savename(f)
+        if os.path.exists(savename):
+            continue
+        with h5py.File(f, 'r') as h5load, h5py.File(savename, 'w') as h5save:
             for pix in pixlist:
                 for alg in alglist:
                     # time.sleep(1)
-                    results_by_file[pix][alg].append(
-                        evaluation.AlgorithmResults.from_hdf5_tracks(
-                        h5f, subgroup_name=subgroup_name[pix], alg_name=alg))
+                    print 'Reading ' + f
+                    res = evaluation.AlgorithmResults.from_hdf5_tracks(
+                        h5load, subgroup_name=subgroup_name[pix], alg_name=alg)
+                    print '  Writing ' + savename
+                    trackio.write_object_to_hdf5(
+                        res, h5save, save_group_name[pix][alg])
         pbar.update(i)
     pbar.finish()
 
-    for pix in pixlist:
-        for alg in alglist:
-            all_results[pix][alg] = results_sum(results_by_file[pix][alg])
+    # for pix in pixlist:
+    #     for alg in alglist:
+    #         all_results[pix][alg] = results_sum(results_by_file[pix][alg])
+    #
+    # pass
 
-    pass
+
+def make_savename(loadname):
+    base, _ = os.path.splitext(loadname)
+    savename = base + '_AR.h5'
+    return savename
 
 
 def results_sum(results_list):
@@ -69,7 +85,6 @@ def results_sum(results_list):
     else:
         # must give it the starting value of an AR object
         return sum(results_list[1:], results_list[0])
-
 
 
 if __name__ == '__main__':
