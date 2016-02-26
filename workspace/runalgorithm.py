42bff0980194593789624932362fc9e305b05f8f
Brian Plimley <brian.plimley@gmail.com>
2016-02-26 00:06:04 -0800

    runalgorithm.py: adds algs into track obj, etc.

--- workspace/runalgorithm.py
+++ workspace/runalgorithm.py
@@ -8,8 +8,10 @@ import ipdb as pdb
 import progressbar
 import time
 
+from etrack.reconstruction.evaluation import AlgorithmResults
 
-def runalgorithm(track_list, reconstruct_list):
+
+def runalgorithm(track_list, reconstruct_dict):
     """
     Inputs:
       track_list: list of m tracks to process
@@ -22,35 +24,61 @@ def runalgorithm(track_list, reconstruct_list):
     progressflag = True
 
     maxm = len(track_list)
-    maxn = len(reconstruct_list)
+    maxn = len(reconstruct_dict)
     result = np.zeros((maxm, maxn))
     errorcount = np.zeros((maxn,))
+    skipcount = np.zeros((maxn,))
 
     if progressflag:
         pbar = progressbar.ProgressBar(
             widgets=[progressbar.Percentage(), ' ', progressbar.Bar(), ' ',
                      progressbar.ETA()], maxval=maxm * maxn)
         pbar.start()
-    for n, reconstruct in enumerate(reconstruct_list):
+    for n, alg_name in enumerate(reconstruct_dict.keys()):
         t1 = time.time()
         for m, track in enumerate(track_list):
             if progressflag:
                 pbar.update(n * maxm + m)
-            try:
-                _, this_info = reconstruct(track)
-                this_da = this_info.alpha_deg - track.g4track.alpha_deg
-                result[m, n] = this_da
-            except IOError:
-                # placeholder except block
-                result[m, n] = np.nan
-                errorcount[n] += 1
+            if alg_name in track.list_algorithms():
+                skipcount[n] += 1
+            else:
+                try:
+                    _, this_info = reconstruct_dict[alg_name](track)
+                    track.add_algorithm(
+                        alg_name, this_info.alpha_deg, this_info.beta_deg)
+                    this_da = this_info.alpha_deg - track.g4track.alpha_deg
+                    result[m, n] = this_da
+                except IOError:
+                    # placeholder except block
+                    result[m, n] = np.nan
+                    errorcount[n] += 1
         t2 = time.time()
         print(
-            'Algorithm {} finished in {} s with {} errors for {} s/track'.format(
-            reconstruct, t2 - t1, errorcount[n], (t2 - t1) / (maxm - errorcount[n])
+            ('Algorithm {} finished in {} s ' +
+             'with {} errors for {} s/track').format(
+                alg_name, t2 - t1, errorcount[n],
+                (t2 - t1) / (maxm - errorcount[n] - skipcount[n])
             ))
 
     if progressflag:
         pbar.finish()
 
     return result
+
+
+def construct_ARdict(track_list, alglist):
+    """
+    Make a dict of algorithms (NOT pixsize/noise!), from the track list.
+    """
+
+    ARdict = {}
+    for alg in alglist:
+        ARdict[alg] = AlgorithmResults.from_track_list(
+            track_list, alg_name=alg)
+
+    return ARdict
+
+
+if False:
+    pdb.set_trace()
+    pass
