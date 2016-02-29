f6b1fa7f13c54566b3c9112c9fc60f85b3cfad92
Brian Plimley <brian.plimley@gmail.com>
2016-02-28 19:29:46 -0800

    hybridtrack.py: meas length is length from old method, if longer

--- reconstruction/hybridtrack.py
+++ reconstruction/hybridtrack.py
@@ -853,9 +853,15 @@ def select_measurement_points2(ridge, options, energy_kev, beta_deg=None,
 
     start = n_points_to_skip_from_diffusion
 
+    start_old, end_old = select_measurement_points(
+        ridge, options, energy_kev, beta_deg=beta_deg, cos_beta=cos_beta)
+
     end1 = 2 * start
     end2 = start + 1.0 / options.position_step_size_pix
-    end = np.maximum(end1, end2)
+    end3 = start + (end_old - start_old)
+    end = np.max([end1, end2, end3])
+    if end > len(ridge):
+        end = len(ridge)
 
     return int(start), int(end)     # these become indices
 
