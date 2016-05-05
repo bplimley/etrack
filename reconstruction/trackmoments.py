b4ccc5e9daf07592f20bd4a3db686986a60f16e3
Brian Plimley <as.white.as.snow@gmail.com>
2016-05-05 12:43:22 -0700

    trackmoments: now a class; basic moments done

--- reconstruction/trackmoments.py
+++ reconstruction/trackmoments.py
@@ -4,23 +4,71 @@ import numpy as np
 
 from etrack.reconstruction import hybridtrack
 
+# Don's summary from November 2015:
+#  (0. separate initial segment)
+#   1. compute moments
+#   2a. determine center of image
+#   2b. recalculate moments relative to center
+#   3a. rotate image to principal axes of distribution
+#      (positive x-axis in direction of motion)
+#   4. find the arc parameters in the principle axis frame
+#   5. find angle of entry, point of entry, arc length, and rate of
+#      charge deposition from arc parameters
 
-def reconstruct(original_image_kev, pixel_size_um=10.5):
-    """
-    """
 
-    options = hybridtrack.ReconstructionOptions(pixel_size_um)
-    info = hybridtrack.ReconstructionInfo()
+class MomentsReconstruction(object):
 
-    hybridtrack.choose_initial_end(original_image_kev, options, info)
+    def __init__(self, original_image_kev, pixel_size_um=10.5):
+        """
+        Init: Load options only
+        """
 
-    segment_initial_end(original_image_kev, options, info)
+        self.original_image_kev = original_image_kev
+        self.options = hybridtrack.ReconstructionOptions(pixel_size_um)
+        self.info = hybridtrack.ReconstructionInfo()
 
-    compute_moments(options, info)
+    def reconstruct(self):
 
-    compute_direction(options, info)
+        hybridtrack.choose_initial_end(
+            self.original_image_kev, self.options, self.info)
 
-    return options, info
+        self.segment_initial_end()
+
+        self.compute_moments()
+
+        self.compute_direction()
+
+    def compute_first_moments(self):
+        clist = CoordinatesList.from_image(self.end_segment_image)
+        T = np.empty((2, 2))
+        for i in xrange(2):
+            for j in xrange(2):
+                if i + j < 2:
+                    T[i, j] = get_moment(clist.x, clist.y, clist.E, i, j)
+        self.first_moments = T
+
+
+class CoordinatesList(object):
+
+    def __init__(self, xlist, ylist, Elist):
+        self.x = xlist
+        self.y = ylist
+        self.E = Elist
+
+    @classmethod
+    def from_image(cls, image, xoffset=0.0, yoffset=0.0):
+        xi = np.array(range(image.shape[0]))
+        yi = np.array(range(image.shape[1]))
+        xi -= xoffset
+        yi -= yoffset
+        xx, yy = np.meshgrid(xi, yi, indexing='ij')
+
+        coordlist = cls(xx.flatten(), yy.flatten(), image.flatten())
+        return coordlist
+
+
+def get_moment(xlist, ylist, elist, r, s):
+    return np.sum(elist * xlist**r * ylist**s)
 
 
 def segment_initial_end(image_kev, options, info):
