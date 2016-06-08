db31a9e2c974fc84bf84fbae80ad51e1477e2855
Brian Plimley <as.white.as.snow@gmail.com>
2016-06-08 16:23:35 -0700

    test_moments: add main funcs, get_tracklist, momentslist

--- reconstruction/test_moments.py
+++ reconstruction/test_moments.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 import h5py
 import pandas as pd
 import time
+import datetime
+import glob
 
 from etrack.reconstruction.trackdata import Track
 import etrack.io.trackio as trackio
@@ -18,13 +20,14 @@ import etrack.reconstruction.trackmoments as tm
 import etrack.visualization.trackplot as tp
 
 
-def tracklist_from_h5(filename, energy_thresh_kev):
+def tracklist_from_h5(filename, energy_thresh):
     """
     Go through a pyml h5 file and make Tracks from pix10_5noise0noise0.
     Save them into a list if their energy is above energy_thresh_kev.
     """
 
     f = h5py.File(filename, 'r')
+    fn = 'pix10_5noise0'
     tracklist = []
     for evtkey in f.keys():
         try:
@@ -42,6 +45,7 @@ def tracklist_from_h5(filename, energy_thresh_kev):
             tracklist.append(t)
 
     return tracklist
+
 
 def test_moments_segmentation(tracklist):
     """
@@ -63,7 +67,7 @@ def test_moments_segmentation(tracklist):
             notimplemented += 1
             continue
 
-        fig = tp.plot_moments_segment(mom.original_image_kev, mom.box)
+        tp.plot_moments_segment(mom.original_image_kev, mom.box)
         titlestr = '#{}, rough_est={}*, start={}, end={}'.format(
             i, mom.rough_est * 180 / np.pi,
             mom.start_coordinates, mom.end_coordinates)
@@ -78,11 +82,98 @@ def test_moments_segmentation(tracklist):
         notimplemented, len(tracklist)))
 
 
-filename = '/media/plimley/TEAM 7B/HTbatch01_pyml/MultiAngle_HT_100_11_py.h5'
-fn = 'pix10_5noise0'
+def momentlist_from_tracklist(tracklist):
+    """
+    get lists of each moment
+    """
 
-energy_thresh = 300     # keV
-print('Compiling tracklist (energy_thresh = {} keV)'.format(energy_thresh))
-tracklist = tracklist_from_h5(filename, energy_thresh)
+    max_length = 10000
 
-test_moments_segmentation(tracklist)
+    first_moments = np.zeros((max_length, 2, 2))
+    central_moments = np.zeros((max_length, 4, 4))
+    rotated_moments = np.zeros((max_length, 4, 4))
+
+    n = 0
+    index_error_count = 0
+    not_implemented_error_count = 0
+
+    print('Performing moments reconstruction...')
+    for t in tracklist:
+        try:
+            mom = tm.MomentsReconstruction(t.image)
+            mom.reconstruct()
+        except IndexError:
+            index_error_count += 1
+            continue
+        except NotImplementedError:
+            not_implemented_error_count += 1
+            continue
+
+        # copy moments
+        for i in xrange(3):
+            for j in xrange(3):
+                if i + j <= 1:
+                    first_moments[n, i, j] = mom.first_moments[i, j]
+                if i + j <= 3:
+                    central_moments[n, i, j] = mom.central_moments[i, j]
+                    rotated_moments[n, i, j] = mom.rotated_moments[i, j]
+        n += 1
+
+    # pdb.set_trace()
+    # remove extra entries
+    first_moments = first_moments.copy()
+    first_moments.resize((n, 2, 2))
+    central_moments = central_moments.copy()
+    central_moments.resize((n, 4, 4))
+    rotated_moments = rotated_moments.copy()
+    rotated_moments.resize((n, 4, 4))
+
+    return first_moments, central_moments, rotated_moments
+
+
+def main1():
+    """
+    plot the bounding box for segmentation
+    """
+
+    filename = '/media/plimley/TEAM 7B/HTbatch01_pyml/MultiAngle_HT_100_11_py.h5'
+    energy_thresh = 300     # keV
+    print('Compiling tracklist (energy_thresh = {} keV)'.format(energy_thresh))
+    tracklist = tracklist_from_h5(filename, energy_thresh)
+
+    test_moments_segmentation(tracklist)
+
+
+def get_tracklist(n_files=10):
+
+    filename = '/media/plimley/TEAM 7B/HTbatch01_pyml/MultiAngle_HT_*_*_py.h5'
+    flist = glob.glob(filename)
+    energy_thresh = 300     # keV
+    tracklist = []
+
+    # get a bunch of tracks: ~250 in energy window per file, ~100 without error
+    for fname in flist[:n_files]:
+        # add items to list. easy but potentially slow way
+        print('Getting tracks from {} at {}...'.format(
+            fname, datetime.datetime.now()))
+        tracklist += tracklist_from_h5(fname, energy_thresh)
+
+    return tracklist
+
+
+def main2():
+    """
+    get the moments lists for histogramming
+    """
+
+    tracklist = get_tracklist(n_files=1)
+
+    # get moments from easy ones
+    # first, central, rotated = momentlist_from_tracklist(tracklist)
+    return momentlist_from_tracklist(tracklist)
+
+    #
+
+
+if __name__ == '__main__':
+    main2()
