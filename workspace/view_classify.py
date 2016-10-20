dbf3ba61d185238e22f68cd2034e472048076ff0
Brian Plimley <as.white.as.snow@gmail.com>
2016-10-20 12:42:45 -0700

    workspace/view_classify.py: interactive script for viewing files

---
+++ workspace/view_classify.py
@@ -0,0 +1,65 @@
+
+
+import os
+import h5py
+import socket
+import matplotlib.pyplot as plt
+
+
+def run_main():
+    """
+    Go through the default file, show events based on user input.
+    """
+
+    loadfile = 'MultiAngle_algs_100_1.h5'
+    loadfull = os.path.join(get_loadpath(), loadfile)
+    prompt = '[#], [n]ext, [p]revious, [q]uit: '
+    pn = 'pix10_5noise15'
+
+    with h5py.File(loadfull, 'r') as f:
+        usr_input = 0
+
+        while not usr_input.startswith('q'):
+            if isinstance(usr_input, int):
+                indnum = usr_input
+            elif usr_input.startswith('n'):
+                indnum += 1
+            elif usr_input.startswith('p'):
+                indnum -= 1
+            indstr = '{:05d}'.format(indnum)
+
+            try:
+                show_event(f[indstr][pn])
+            except KeyError:
+                print('### KeyError on {}/{}'.format(indstr, pn))
+
+            usr_input = raw_input(prompt)
+
+
+def show_event(pn):
+    """
+    Plot stuff for one event. pn is the h5py group of the pixelnoise.
+    """
+
+    pass
+
+
+def get_loadpath():
+    """
+    Return a system-dependent path for the file to load.
+    """
+
+    if socket.gethostname() == 'plimley-Vostro-mint17':
+        # LBL desktop
+        loadpath = '/media/plimley/TEAM 7B/algs_10.5_batch01'
+    elif socket.gethostname() == 'plimley-zenbook-mint':
+        # laptop
+        loadpath = '/home/plimley/Documents/research/data/algs_10.5_batch01'
+    else:
+        raise RuntimeError("I don't know what system I'm on")
+
+    return loadpath
+
+
+if __name__ == '__main__':
+    run_main()
