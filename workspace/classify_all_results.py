bc82a5c8e1d113b9b81c4b83cbf773b269473111
Brian Plimley <as.white.as.snow@gmail.com>
2016-11-02 13:47:54 -0700

    classify_all_results.py: draft without error handling

---
+++ workspace/classify_all_results.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import ipdb as pdb
+import numpy as np
+import h5py
+import time
+import glob
+import os
+import multiprocessing
+import psutil
+import progressbar
+import socket
+
+from etrack.reconstruction.trackdata import Track
+import etrack.io.trackio as trackio
+import etrack.reconstruction.trackmoments as tm
+import etrack.reconstruction.classify as cl
+from etrack.workspace.filejob import JobOptions, vprint
+
+# runs after classify_all.py
+# go through all the MultiAngle_algs_*.h5,
+#   collects the results numbers, and saves into a results file.
+
+
+# copied from filejob.py
+def run_main():
+
+    multi_flag, _, loadpath, savepath, loadglob, saveglob, v, n_proc = (
+        file_vars())
+
+    if not os.path.isdir(savepath):
+        os.mkdir(savepath)
+
+    flist = glob.glob(os.path.join(loadpath, loadglob))
+    flist.sort()
+
+    print('Found {} files to load'.format(len(flist)))
+
+    if multi_flag:
+        p = multiprocessing.Pool(processes=n_proc, maxtasksperchild=5)
+        p.map(runfile, flist, chunksize=5)
+    else:
+        [runfile(f) for f in flist]
+
+
+def file_vars():
+    """
+    Define file path and globs here. Also server flag, and verbosity.
+
+    Gets loaded in run_main() as well as runfile(loadname).
+    """
+
+    multi_flag = False   # run in parallel - turn off to debug
+    if socket.gethostname() == 'plimley-Vostro-mint17':
+        server_flag = False
+    elif socket.gethostname().startswith('n0'):
+        server_flag = True
+
+    if server_flag:
+        n_threads = 11
+        loadpath = '/global/home/users/bcplimley/multi_angle/algs_10.5_batch01'
+        savepath = '/global/home/users/bcplimley/multi_angle/clresults_10.5_batch01'
+    else:
+        # LBL desktop
+        n_threads = 4
+        loadpath = '/media/plimley/TEAM 7B/algs_10.5_batch01'
+        savepath = '/media/plimley/TEAM 7B/clresults_10.5_batch01'
+    loadglob = 'MultiAngle_algs_*_*.h5'
+    saveglob = 'MultiAngle_results_*_*.h5'
+
+    v = 1   # verbosity
+
+    return (multi_flag, server_flag, loadpath, savepath, loadglob, saveglob, v,
+            n_threads)
+
+
+def runfile(loadname):
+    """
+    To use:
+      1. make a copy in your script
+      2. edit this function name to e.g. runmyjob(loadname)
+      3. edit work function name from main_work_function below
+      4. edit paths, globs, and flags
+      5. in the main script:
+         flist = glob.glob(os.path.join(loadpath, loadglob))
+         p = multiprocessing.Pool(processes=n, maxtasksperchild=25)
+         p.map(runmyjob, flist, chunksize=25)
+    """
+
+    # drop the path part of loadname, if it is given
+    multi_flag, server_flag, loadpath, savepath, loadglob, saveglob, v, _ = (
+        file_vars())
+
+    in_place_flag = False
+    phflag = True
+    doneflag = False
+
+    # setup
+    opts = JobOptions(
+        loadpath=loadpath, loadglob=loadglob,
+        savepath=savepath, saveglob=saveglob,
+        in_place_flag=in_place_flag, phflag=phflag, doneflag=doneflag)
+    # decide to skip or not; construct full filenames
+    loadfile, savefile = opts.pre_job_tasks(loadname)
+    if loadfile is not None and savefile is not None:
+        # do the work
+        get_results(loadfile, savefile, v)
+        # clean up
+        opts.post_job_tasks(loadname)
+
+
+def get_results(loadfile, savefile, v):
+    """
+    Get all the important numbers for each event.
+
+    (basic info:)
+        Etot
+        Edep
+        Etrack
+        alpha_true
+        beta_true
+        errorcode
+        file
+        ind
+    (ridge following result:)
+        alpha_rf
+    (moments result:)
+        alpha_m
+    (general rejection parameters:)
+        min_end_energy
+        max_end_energy
+        n_ends
+    (moments rejection parameters:)
+        phi
+        edge_pixels
+        edge_segments
+    (Monte Carlo rejection parameters:)
+        overlap flag
+        wrong_end flag
+        early_scatter flag
+        total_scatter_angle
+    """
+
+    progressflag = not file_vars()[0]
+
+    pn = 'pix10_5noise15'
+    HTname = 'python HT v1.52'
+    MTname = 'moments v1.0'
+
+    vprint(v, 1, 'Starting {} at {} with {}% mem usage'.format(
+        loadfile, time.ctime(), psutil.virtual_memory().percent))
+
+    datalen = 1000
+    # basic info
+    energy_tot_kev = np.nan * np.ones(datalen)
+    energy_dep_kev = np.nan * np.ones(datalen)
+    energy_track_kev = np.nan * np.ones(datalen)
+    alpha_true_deg = np.nan * np.ones(datalen)
+    beta_true_deg = np.nan * np.ones(datalen)
+    errorcode = np.nan * np.ones(datalen)
+    filename = np.nan * np.ones(datalen)
+    fileind = np.nan * np.ones(datalen)
+    # ridge following
+    alpha_ridge_deg = np.nan * np.ones(datalen)
+    # moments
+    alpha_moments_deg = np.nan * np.ones(datalen)
+    # rejection
+    min_end_energy_kev = np.nan * np.ones(datalen)
+    max_end_energy_kev = np.nan * np.ones(datalen)
+    n_ends = np.nan * np.ones(datalen)
+    phi_deg = np.nan * np.ones(datalen)
+    edge_pixels = np.nan * np.ones(datalen)
+    edge_segments = np.nan * np.ones(datalen)
+    # monte carlo
+    overlap_flag = np.nan * np.ones(datalen)
+    wrong_end_flag = np.nan * np.ones(datalen)
+    early_scatter_flag = np.nan * np.ones(datalen)
+    total_scatter_angle_deg = np.nan * np.ones(datalen)
+
+    try:
+        with h5py.File(loadfile, 'r', driver='core') as f, h5py.File(
+                savefile, 'a', driver='core') as h5save:
+            if progressflag:
+                pbar = progressbar.ProgressBar(
+                    widgets=[progressbar.Percentage(), ' ',
+                             progressbar.Bar(), ' ',
+                             progressbar.ETA()], maxval=len(f))
+                pbar.start()
+
+            for ind in xrange(datalen):
+                indstr = '{:05d}'.format(ind)
+                trkstr = indstr
+                clstr = 'cl_' + indstr
+                momstr = 'mom_' + indstr
+
+                this_errorcode = 0
+
+                # try:
+                this_trk = Track.from_hdf5(f[trkstr][pn])
+                # except KeyError:
+                #     pass
+                # except trackio.InterfaceError:
+                #     pass
+                energy_tot_kev[ind] = this_trk.g4track.energy_tot_kev
+                energy_dep_kev[ind] = this_trk.g4track.energy_dep_kev
+                energy_track_kev[ind] = this_trk.energy_kev
+                alpha_true_deg[ind] = this_trk.g4track.alpha_deg
+                beta_true_deg[ind] = this_trk.g4track.beta_deg
+                filename[ind] = os.path.split(loadfile)[-1]
+                fileind[ind] = ind
+                alpha_ridge_deg[ind] = this_trk[HTname].alpha_deg
+                alpha_moments_deg[ind] = this_trk[MTname].alpha_deg
+
+                # try:
+                this_cl = cl.Classifier.from_hdf5(f[clstr][pn])
+                # except KeyError:
+                #     pass
+                min_end_energy_kev[ind] = this_cl.min_end_energy
+                max_end_energy_kev[ind] = this_cl.max_end_energy
+                n_ends[ind] = this_cl.n_ends
+                overlap_flag[ind] = this_cl.overlap
+                wrong_end_flag[ind] = this_cl.wrong_end
+                early_scatter_flag[ind] = this_cl.early_scatter
+                total_scatter_angle_deg[ind] = (
+                    this_cl.total_scatter_angle / np.pi * 180)
+
+                # try:
+                this_mom = tm.MomentsReconstruction.from_hdf5(
+                    f[momstr][pn])
+                # except KeyError:
+                #     pass
+                phi_deg[ind] = this_mom.phi / np.pi * 180
+                edge_pixels[ind] = this_mom.edge_pixel_count
+                edge_segments[ind] = this_mom.edge_pixel_segments
+
+                errorcode[ind] = this_errorcode
+
+    except NotImplementedError:
+        pass
+
+
+if __name__ == '__main__':
+    run_main()
+
+    if False:
+        pdb.set_trace()
