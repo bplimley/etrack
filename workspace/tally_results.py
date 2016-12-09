01c1e453bb12d107512e3b393a6df7477e99218f
Brian Plimley <as.white.as.snow@gmail.com>
2016-12-08 16:44:55 -0800

    tally_results.py: construct_logical, condition_lookup

---
+++ workspace/tally_results.py
@@ -0,0 +1,168 @@
+# tally_results.py
+#
+# tally results from compiled_results.h5 (result of compile_classify.py)
+#
+# Classification cases:
+#  0: multiplicity / bad segmentation / corrupted file
+#  1: escape. endpoint not found (or rejected)
+#  2: escape. endpoint accepted. wrong end. both reject
+#  3: escape. endpoint accepted. wrong end. moments accepts
+#  4: escape. endpoint accepted. wrong end. ridge accepts
+#  5: escape. endpoint accepted. wrong end. both accept
+#  6: escape. endpoint accepted. right end. both reject
+#  7: escape. endpoint accepted. right end. moments accepts
+#  8: escape. endpoint accepted. right end. ridge accepts
+#  9: escape. endpoint accepted. right end. both accept
+# 10: contained. endpoint not found (or rejected)
+# 11: contained. endpoint accepted. wrong end. both reject
+# 12: contained. endpoint accepted. wrong end. moments accepts
+# 13: contained. endpoint accepted. wrong end. ridge accepts
+# 14: contained. endpoint accepted. wrong end. both accept
+# 15: contained. endpoint accepted. right end. both reject. early scatter
+# 16: contained. endpoint accepted. right end. both reject. no early scatter
+# 17: contained. endpoint accepted. right end. moments accepts. early scatter
+# 18: contained. endpoint accepted. right end. moments accepts. no early scattr
+# 19: contained. endpoint accepted. right end. ridge accepts. early scatter
+# 20: contained. endpoint accepted. right end. ridge accepts. no early scatter
+# 21: contained. endpoint accepted. right end. both accept. early scatter
+# 22: contained. endpoint accepted. right end. both accept. no early scatter
+
+from __future__ import print_function
+import numpy as np
+import h5py
+import os
+import ipdb as pdb
+
+from compile_classify import data_variable_list
+
+TEST_KEY = 'energy_tot_kev'
+
+# thresholds
+ESCAPE_KEV = 2.0
+MAX_END_MIN_KEV = 45.0
+MIN_END_MAX_KEV = 25.0
+PHI_MAX = np.pi / 2
+EDGE_PIXELS_MAX = 4
+EDGE_SEGMENTS_MAX = 1
+
+
+def main():
+    filepath = '/media/plimley/TEAM 7B/clresults_10.5_batch01'
+    filename = 'compiled_results.h5'
+    datadict = get_data_dict(os.path.join(filepath, filename))
+
+
+class Condition(object):
+    """Simple container to contain the condition key, operator, and value."""
+
+    def __init__(self, key, val, op=None):
+        self.key = key
+        self.value = val
+        if op is None:
+            self.op = '=='
+        else:
+            self.op = op
+
+
+def construct_logical(datadict, cond_list):
+    """
+    Construct a logical vector based on a condition list.
+    """
+
+    lg = np.ones_like(datadict[TEST_KEY]).astype(bool)
+    for cond in cond_list:
+        lg = lg & (datadict[cond.key] == cond.value)
+
+    return lg
+
+
+def condition_lookup(casenum):
+    """
+    Conditions which describe one case number from the classification chart.
+    """
+
+    if casenum == 0:
+        #  0: multiplicity / bad segmentation / corrupted file
+        cond_list = [Condition('trk_errorcode', 0, op='!=')]
+    else:
+        cond_list = [Condition('trk_errorcode', 0)]
+
+    if casenum >= 1 and casenum <= 9:
+        # escape
+        cond_list.append(Condition('is_escape', 1))
+    else:
+        # contained
+        cond_list.append(Condition('is_escape', 0))
+
+    if casenum == 1 or casenum == 10:
+        # endpoint not found (or, not accepted)
+        cond_list.append(Condition('endpoint_accept', 0))
+    elif casenum > 0:
+        # endpoint accepted
+        cond_list.append(Condition('endpoint_accept', 1))
+
+    if (casenum >= 2 and casenum <= 5) or (casenum >= 11 and casenum <= 14):
+        # wrong end
+        cond_list.append(Condition('wrong_end_flag', 1))
+    elif casenum > 0:
+        # right end
+        cond_list.append(Condition('wrong_end_flag', 0))
+
+    if casenum in (2, 6, 11, 15, 16):
+        # both reject
+        cond_list.append(Condition('ridge_accept', 0))
+        cond_list.append(Condition('moments_accept', 0))
+    elif casenum in (3, 7, 12, 17, 18):
+        # moments accepts
+        cond_list.append(Condition('ridge_accept', 0))
+        cond_list.append(Condition('moments_accept', 1))
+    elif casenum in (4, 8, 13, 19, 20):
+        # ridge accepts
+        cond_list.append(Condition('ridge_accept', 1))
+        cond_list.append(Condition('moments_accept', 0))
+    elif casenum in (5, 9, 14, 21, 22):
+        # both accept
+        cond_list.append(Condition('ridge_accept', 1))
+        cond_list.append(Condition('moments_accept', 1))
+
+    if casenum in (15, 17, 19, 21):
+        # early scatter
+        cond_list.append(Condition('early_scatter_flag', 1))
+    elif casenum in (16, 18, 20, 22):
+        # no early scatter
+        cond_list.append(Condition('early_scatter_flag', 0))
+
+
+def get_data_dict(filename):
+    """
+    Load the hdf5 file and pull all the data from it into a dict.
+    """
+
+    varlist = data_variable_list()
+    datadict = {}
+
+    with h5py.File(filename, 'r') as f:
+        datalen = f[TEST_KEY].shape[0]
+        for key in varlist:
+            datadict[key] = np.empty(datalen)
+            f[key].read_direct(datadict[key])
+
+    # make implicit flags/parameters explicit
+    datadict['no_trk_error'] = (datadict['trk_errorcode'] == 0)
+    datadict['is_contained'] = np.abs(datadict['energy_tot_kev'] -
+                                      datadict['energy_dep_kev']) < ESCAPE_KEV
+    datadict['endpoint_accept'] = (
+        (datadict['max_end_energy_kev'] > MAX_END_MIN_KEV) &
+        (datadict['min_end_energy_kev'] < MIN_END_MAX_KEV) &
+        (datadict['n_ends'] > 0))
+    datadict['moments_accept'] = (
+        (np.abs(datadict['phi']) < PHI_MAX) &
+        (datadict['edge_pixels'] <= EDGE_PIXELS_MAX) &
+        (datadict['edge_segments'] <= EDGE_SEGMENTS_MAX))
+    datadict['ridge_accept'] = np.logical_not(
+        np.isnan(datadict['alpha_ridge_deg']))
+    return datadict
+
+
+if __name__ == '__main__':
+    main()
