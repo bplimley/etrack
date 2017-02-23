13722fce36842643373c0029cf08af96bf80a1fd
Brian Plimley <as.white.as.snow@gmail.com>
2017-02-23 12:17:41 -0800

    tabata.py refactored (API changes)

--- physics/tabata.py
+++ physics/tabata.py
@@ -1,5 +1,17 @@
 """
 Compute electron ranges using analytical formulae in Tabata's papers.
+
+Tabata 1996a:
+Tatsuo Tabata, Pedro Andreo, Kunihiko Shinoda.
+"An analytic formula for the extrapolated range of electrons in condensed
+materials."
+Nucl. Instr. Meth. B 119 (1996) 463-470.
+
+Tabata 2002:
+Tatsuo Tabata, Vadim Moskvin, Pedro Andreo, Valentin Lazurik, Yuri Rogov.
+"Extrapolated ranges of electrons determined from transmission and projected-
+range straggling curves."
+Rad. Phys. and Chem. 64 (2002) 161-167.
 """
 
 from __future__ import print_function
@@ -8,208 +20,364 @@ import numpy as np
 
 ELECTRON_REST_ENERGY_MEV = 0.510999
 
+MIN_ENERGY_1996A_KEV = 1    # Tabata 1996a p. 2
+SEMI_MIN_ENERGY_1996A_KEV = 10  # Tabata 1996a p. 2 (shell effects)
+MAX_ENERGY_1996A_KEV = 100e3    # Tabata 1996a p. 2
+MIN_ENERGY_2002_KEV = 10    # Tabata 2002 sec 2.3
+MAX_ENERGY_2002_KEV = 50e3  # Tabata 2002 sec 1; 2.1
 
-def compute_deviation_factor_1996a(E_Mev, Z):
-    E_Mev = float(E_Mev)
-    t0 = E_Mev / ELECTRON_REST_ENERGY_MEV
-    # constants from fit: Tabata 1996a Table 1
-    B = (
-        0.0,       # (0)
-        0.3879,     # 1
-        0.2178,     # 2
-        0.4541,     # 3
-        0.03068,    # 4
-        3.326e-16,  # 5
-        13.24,      # 6
-        1.316,      # 7
-        14.03,      # 8
-        0.7406,     # 9
-        4.294e-3,   # 10
-        1.684,      # 11
-        0.2264,     # 12
-        0.6127,     # 13
-        0.1207,     # 14
-    )
-    # Tabata 1996a equations 3--8
-    a = (
-        0.0,                            # (0)
-        B[1] * Z**B[2],                  # 1
-        B[3] + B[4]*Z,                   # 2
-        B[5] * Z**(B[6] - B[7]*np.log(Z)),  # 3
-        B[8] / Z**(B[9]),                # 4
-        B[10]*Z**(B[11] - B[12]*np.log(Z)),  # 5
-        B[13] * Z**B[14],                # 6
-    )
-    # Tabata 1996a equation 2
-    deviationFactor = 1 / (a[1] + a[2]/(1 + a[3]/t0**a[4] + a[5]*t0**a[6]))
-    return deviationFactor
-
-
-def compute_deviation_factor_2002(E_Mev, Z):
-    E_Mev = float(E_Mev)
-    t0 = E_Mev / ELECTRON_REST_ENERGY_MEV
-    # constants from fit: Tabata 2002 Table 2
-    B = (
-        0.0,       # (0)
-        0.2946,     # 1
-        0.2740,     # 2
-        18.4,       # 3
-        3.457,      # 4
-        1.377,      # 5
-        6.59,       # 6
-        2.414,      # 7
-        1.094,      # 8
-        0.05242,    # 9
-        0.3709,     # 10
-        0.958,      # 11
-        2.02,       # 12
-        1.099,      # 13
-        0.2808,     # 14
-        0.2042,     # 15
-    )
-    # Tabata 2002 equations 9--14
-    a = (
-        0.0,                                   # (0)
-        B[1] * Z**B[2],                         # 1
-        B[3] * Z**(-B[4] + B[5]*np.log(Z)),  # 2
-        B[6] * Z**(-B[7] + B[8]*np.log(Z)),  # 3
-        B[9] * Z**(B[10]),                      # 4
-        B[11] * Z**(-B[12]+B[13]*np.log(Z)),  # 5
-        B[14] * Z**B[15],                       # 6
-    )
-    # Tabata 2002 equation 8 (identical to 1996a eq 2)
-    deviationFactor = 1 / (a[1] + a[2]/(1 + a[3]/t0**a[4] + a[5]*t0**a[6]))
-    return deviationFactor
+# paper notation is 1-indexed. so here, elements at index 0 are placeholders
+TABATA_1996A_TABLE_1 = (
+    0.0,        # (0)
+    0.3879,     # 1
+    0.2178,     # 2
+    0.4541,     # 3
+    0.03068,    # 4
+    3.326e-16,  # 5
+    13.24,      # 6
+    1.316,      # 7
+    14.03,      # 8
+    0.7406,     # 9
+    4.294e-3,   # 10
+    1.684,      # 11
+    0.2264,     # 12
+    0.6127,     # 13
+    0.1207,     # 14
+)
+TABATA_1996A_TABLE_2 = (
+    0.0,        # (0)
+    3.600,      # 1
+    0.9882,     # 2
+    1.191e-3,   # 3
+    0.8622,     # 4
+    1.02501,    # 5
+    1.0803e-4,  # 6
+    0.99628,    # 7
+    1.303e-4,   # 8
+    1.02441,    # 9
+    1.2986e-4,  # 10
+    1.030,      # 11
+    1.110e-2,   # 12
+    1.10e-6,    # 13
+    0.959,      # 14
+)
+TABATA_2002_TABLE_2 = (
+    0.0,        # (0)
+    0.2946,     # 1
+    0.2740,     # 2
+    18.4,       # 3
+    3.457,      # 4
+    1.377,      # 5
+    6.59,       # 6
+    2.414,      # 7
+    1.094,      # 8
+    0.05242,    # 9
+    0.3709,     # 10
+    0.958,      # 11
+    2.02,       # 12
+    1.099,      # 13
+    0.2808,     # 14
+    0.2042,     # 15
+)
 
 
-def return_unity(E_Mev, Z):
-    return 1.0
+class TabataError(Exception):
+    """General error for Tabata module."""
+    pass
 
 
-def extrapolated_range(Ekev, Z, A, I_EV, density, ref="2002"):
-
-    def get_ref_mode(ref):
-        if str(ref) == '2002':
-            return '2002'
-        elif str(ref) == '1996' or str(ref) == '1996a':
-            return '1996a'
-        elif str(ref).upper() == 'CSDA':
-            return 'CSDA'
-
-    def get_energy_range(refMode):
-        # (min, semimin, max)
-        # energies all in keV
-        if refMode == '2002':
-            MIN_ENERGY = 10         # 10 keV from Tabata 2002 sec 2.3
-            MAX_ENERGY = 50e3       # 50 MeV from Tabata 2002 sec 1; 2.1
-            if any(Ekev < MIN_ENERGY):
-                print("Range is inaccurate below 10 keV. Try Tabata 1996a.")
-            if any(Ekev > MAX_ENERGY):
-                print("Range is inaccurate above 50 MeV. Try Tabata 1996a.")
-        elif refMode == '1996a' or refMode == 'CSDA':
-            MIN_ENERGY = 1          # 1 keV from Tabata 1996a p. 2
-            SEMI_MIN_ENERGY = 10    # 10 keV from p. 2. (shell effects)
-            MAX_ENERGY = 100e3      # 100 MeV from Tabata 1996a p. 2
-            if any(Ekev < MIN_ENERGY):
-                print("Range is inaccurate below 1 keV.")
-            if any(Ekev > MIN_ENERGY and Ekev < SEMI_MIN_ENERGY):
-                print("Range is less accurate below 10 keV, " +
-                      "because shell effects are ignored.")
-            if any(Ekev > MAX_ENERGY):
-                print("Range is inaccurate above 100 MeV.")
-
-    def get_deviation_function(refMode):
-        # return function. google says you can do this.
-        if refMode == '2002':
-            return compute_deviation_factor_2002
-        elif refMode == '1996a':
-            return compute_deviation_factor_1996a
-        elif refMode == 'CSDA':
-            return return_unity
-
-    # input handling
-    Ekev = float(Ekev)
-    I_EV = float(I_EV)
-    density = float(density)
-    refMode = get_ref_mode(ref)
-    deviationFunction = get_deviation_function(refMode)
-
-    E_Mev = Ekev / 1e3
-    CSDA_rangeGCm2 = CSDA_range(E_Mev, Z, A, I_EV)
-    CSDA_rangeCm = CSDA_rangeGCm2 / density
-    CSDA_rangeUm = CSDA_rangeCm * 1e4
-
-    rangeUm = deviationFunction(E_Mev, Z) * CSDA_rangeUm
-    return rangeUm
+class EnergyOutOfRange(TabataError):
+    """Energy value out of bounds for calculation."""
+    pass
 
 
-def CSDA_range(E_Mev, Z, A, I_EV):
-    # make sure energy is a float
-    E_Mev = float(E_Mev)
+def extrapolated_range_gcm2(energy_keV, Z, A, I_eV, ref=None):
+    """Compute electron extrapolated range with Tabata's analytical formulae.
 
-    I = I_EV / (1e6*ELECTRON_REST_ENERGY_MEV)
-    t0 = E_Mev / ELECTRON_REST_ENERGY_MEV
+    Args:
+      energy_keV: initial electron energy [keV]
+      Z: atomic number of material
+      A: atomic weight of material [AMU]
+      I_eV: mean excitation energy of material [eV]
+      ref: may be '1996a', '2002', 'CSDA', or None.
+        '1996a': calculate extrapolated range using 1996a detour factor.
+        '2002': calculate extrapolated range using 2002 detour factor.
+        'CSDA': calculate CSDA range instead of extrapolated range.
+        None: calculated extrapolated range using better paper (usually 2002).
+        [Default: None]
 
-    # Tabata 1996a Table 2
-    #   0th element is a placeholder, so that the others line up
-    #   with the subscripts in Tabata's paper
-    D = (
-        0.0,        # (0)
-        3.600,       # 1
-        0.9882,      # 2
-        1.191e-3,    # 3
-        0.8622,      # 4
-        1.02501,     # 5
-        1.0803e-4,   # 6
-        0.99628,     # 7
-        1.303e-4,    # 8
-        1.02441,     # 9
-        1.2986e-4,   # 10
-        1.030,       # 11
-        1.110e-2,    # 12
-        1.10e-6,     # 13
-        0.959,       # 14
-    )
+    Returns:
+      float of the computed extrapolated range, in g/cm^2.
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds of accuracy.
+    """
+
+    energy_keV = float(energy_keV)
+    energy_MeV = energy_keV / 1e3
+    I_eV = float(I_eV)
+
+    if ref is None:
+        ref = '2002'
+        if ((ref > MAX_ENERGY_2002_KEV and ref <= MAX_ENERGY_1996A_KEV) or
+                (ref < MIN_ENERGY_2002_KEV and ref >= MIN_ENERGY_1996A_KEV)):
+            ref = '1996a'
+
+    if ref == '1996' or ref.lower() == '1996a':
+        _check_energy_1996a(energy_keV)
+        f_d = _detour_factor_1996a(energy_MeV, Z)
+    elif ref == '2002':
+        _check_energy_2002(energy_keV)
+        f_d = _detour_factor_2002(energy_MeV, Z)
+    elif ref.upper() == 'CSDA':
+        _check_energy_1996a(energy_keV)
+        f_d = 1.
+    else:
+        raise ValueError("ref should be '1996a', '2002', 'CSDA', or None")
+
+    range_gcm2 = f_d * CSDA_range_gcm2(energy_MeV, Z, A, I_eV)
+
+    return range_gcm2
+
+
+def extrapolated_range_mm(energy_keV, Z, A, I_eV, density_gcm3, ref=None):
+    """Compute electron extrapolated range with Tabata's analytical formulae.
+
+    Args:
+      energy_keV: initial electron energy [keV]
+      Z: atomic number of material
+      A: atomic weight of material [AMU]
+      I_eV: mean excitation energy of material [eV]
+      density_gcm3: density of material [g/cm^3]
+      ref: may be '1996a', '2002', 'CSDA', or None.
+        '1996a': calculate extrapolated range using 1996a detour factor.
+        '2002': calculate extrapolated range using 2002 detour factor.
+        'CSDA': calculate CSDA range instead of extrapolated range.
+        None: calculated extrapolated range using better paper (usually 2002).
+        [Default: None]
+
+    Returns:
+      float of the computed extrapolated range, in mm.
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds of accuracy.
+    """
+
+    range_gcm2 = extrapolated_range_gcm2(energy_keV, Z, A, I_eV, ref=None)
+    range_mm = gcm2_to_mm(range_gcm2, density_gcm3)
+    return range_mm
+
+
+def CSDA_range_gcm2(energy_MeV, Z, A, I_eV):
+    """Compute electron CSDA range, using analytical formula in Tabata 1996a.
+
+    Args:
+      energy_MeV: initial electron energy [MeV]
+      Z: atomic number of material
+      A: atomic weight of material [AMU]
+      I_eV: mean excitation energy of material [eV]
+
+    Returns:
+      float of the CSDA range in g/cm^2.
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds of accuracy.
+    """
+
+    energy_MeV = float(energy_MeV)
+
+    I = I_eV / (1e6 * ELECTRON_REST_ENERGY_MEV)
+    t0 = energy_MeV / ELECTRON_REST_ENERGY_MEV
+
+    d = TABATA_1996A_TABLE_2
     # Tabata 1996a equations 13--19
-    #   0th element is a placeholder again
     c = (
-        0.0,              # (0)
-        D[1]*A / Z**D[2],  # 1
-        D[3]*Z**D[4],      # 2
-        D[5] - D[6]*Z,     # 3
-        D[7] - D[8]*Z,     # 4
-        D[9] - D[10]*Z,    # 5
-        D[11]/Z**D[12],    # 6
-        D[13]*Z**D[14],    # 7
+        0.0,                    # (0)
+        d[1] * A / Z**d[2],     # 1
+        d[3] * Z**d[4],         # 2
+        d[5] - d[6] * Z,        # 3
+        d[7] - d[8] * Z,        # 4
+        d[9] - d[10] * Z,       # 5
+        d[11] / Z**d[12],       # 6
+        d[13] * Z**d[14],       # 7
     )
     # Tabata 1996a equation 12
-    B = np.log((t0 / (I + c[7]*t0))**2) + np.log(1 + t0/2)
+    B = np.log((t0 / (I + c[7] * t0))**2) + np.log(1 + t0/2)
     # Tabata 1996a equation 11
-    CSDA_rangeGCm2 = c[1] / B * (
-        np.log(1+c[2]*t0**c[3])/c[2] -
-        c[4]*t0**c[5]/(1 + c[6]*t0)
-    )
-    return CSDA_rangeGCm2
+    range_gcm2 = c[1] / B * (
+        np.log(1 + c[2] * t0**c[3]) / c[2] -
+        c[4] * t0**c[5] / (1 + c[6] * t0))
+
+    return range_gcm2
 
 
-def extrapolated_rangeSi(E_kev, ref="2002"):
-    E_kev = float(E_kev)
-    return extrapolated_range(E_kev, 14, 28.085, 171, 2.329, ref)
+def CSDA_range_mm(energy_MeV, Z, A, I_eV, density_gcm3):
+    """Compute electron CSDA range, using analytical formula in Tabata 1996a.
 
+    Args:
+      energy_MeV: initial electron energy [MeV]
+      Z: atomic number of material
+      A: atomic weight of material [AMU]
+      I_eV: mean excitation energy of material [eV]
+      density_gcm3: density of material [g/cm^3]
 
-def NIST_webbook_Xe_density(pressure_bar):
+    Returns:
+      float of the CSDA range in mm.
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds of accuracy.
     """
-    Lookup table of NIST WebBook's density listing for xenon at various
+
+    range_gcm2 = CSDA_range_gcm2(energy_MeV, Z, A, I_eV)
+    range_mm = gcm2_to_mm(range_gcm2)
+    return range_mm
+
+
+def gcm2_to_mm(mass_thickness_gcm2, density_gcm3):
+    """Convert a mass thickness to a length.
+
+    Args:
+      mass_thickness_gcm2: mass thickness [g/cm^2]
+      density_gcm3: density of material [g/cm^3]
+
+    Returns:
+      float of the length in mm
+    """
+
+    density_gcm3 = float(density_gcm3)
+    length_cm = mass_thickness_gcm2 / density_gcm3
+    length_mm = length_cm * 10
+
+    return length_mm
+
+
+def _check_energy_1996a(energy_keV):
+    """Check for energy value out of bounds for Tabata 1996a.
+
+    Args:
+      energy_keV: initial electron energy [keV]
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds
+    """
+
+    if energy_keV < MIN_ENERGY_1996A_KEV:
+        raise EnergyOutOfRange(
+            'Range is inaccurate below {} keV'.format(MIN_ENERGY_1996A_KEV))
+    elif energy_keV < SEMI_MIN_ENERGY_1996A_KEV:
+        print(
+            'Range is less accurate below {} keV, '.format(
+                SEMI_MIN_ENERGY_1996A_KEV) +
+            'because shell effects are ignored')
+    elif energy_keV > MAX_ENERGY_1996A_KEV:
+        raise EnergyOutOfRange(
+            'Range is inaccurate above {} keV'.format(MAX_ENERGY_1996A_KEV))
+
+
+def _check_energy_2002(energy_keV):
+    """Check for energy value out of bounds for Tabata 2002.
+
+    Args:
+      energy_keV: initial electron energy [keV]
+
+    Raises:
+      EnergyOutOfRange: if energy is out of bounds
+    """
+
+    if energy_keV < MIN_ENERGY_2002_KEV:
+        raise EnergyOutOfRange(
+            'Range is inaccurate below {} keV'.format(MIN_ENERGY_1996A_KEV))
+    elif energy_keV > MAX_ENERGY_2002_KEV:
+        raise EnergyOutOfRange(
+            'Range is inaccurate above {} keV'.format(MAX_ENERGY_1996A_KEV))
+
+
+def _detour_factor_1996a(energy_MeV, Z):
+    """Ratio of extrapolated range to CSDA range, according to Tabata 1996a.
+
+    Args:
+      energy_MeV: initial electron energy [MeV]
+      Z: atomic number of material
+
+    Returns:
+      a float representing (extrapolated range) / (CSDA range)
+    """
+
+    energy_MeV = float(energy_MeV)
+    t0 = energy_MeV / ELECTRON_REST_ENERGY_MEV
+    b = TABATA_1996A_TABLE_1
+    # Tabata 1996a equations 3--8
+    a = (
+        0.0,                                        # (0)
+        b[1] * Z**b[2],                             # 1
+        b[3] + b[4] * Z,                            # 2
+        b[5] * Z**(b[6] - b[7] * np.log(Z)),        # 3
+        b[8] / Z**(b[9]),                           # 4
+        b[10] * Z**(b[11] - b[12] * np.log(Z)),     # 5
+        b[13] * Z**b[14],                           # 6
+    )
+    # Tabata 1996a equation 2
+    detour_factor = 1 / (
+        a[1] + a[2] / (
+            1 + a[3] / t0**a[4] + a[5] * t0**a[6]))
+
+    return detour_factor
+
+
+def _detour_factor_2002(energy_MeV, Z):
+    """Ratio of extrapolated range to CSDA range, according to Tabata 2002.
+
+    Args:
+      energy_MeV: initial electron energy [MeV]
+      Z: atomic number of material
+
+    Returns:
+      a float representing (extrapolated range) / (CSDA range)
+    """
+
+    energy_MeV = float(energy_MeV)
+    t0 = energy_MeV / ELECTRON_REST_ENERGY_MEV
+    b = TABATA_2002_TABLE_2
+    # Tabata 2002 equations 9--14
+    a = (
+        0.0,                                        # (0)
+        b[1] * Z**b[2],                             # 1
+        b[3] * Z**(-b[4] + b[5] * np.log(Z)),       # 2
+        b[6] * Z**(-b[7] + b[8] * np.log(Z)),       # 3
+        b[9] * Z**(b[10]),                          # 4
+        b[11] * Z**(-b[12] + b[13] * np.log(Z)),    # 5
+        b[14] * Z**b[15],                           # 6
+    )
+    # Tabata 2002 equation 8 (identical to 1996a eq 2)
+    detour_factor = 1 / (
+        a[1] + a[2] / (
+            1 + a[3] / t0**a[4] + a[5] * t0**a[6]))
+
+    return detour_factor
+
+
+# convenience functions
+
+def extrapolated_range_Si(energy_keV, ref="2002"):
+    energy_keV = float(energy_keV)
+    range_mm = extrapolated_range_mm(energy_keV, 14, 28.085, 171, 2.329, ref)
+    range_um = range_mm * 1e3
+    return range_um
+
+
+def NIST_webbook_Xe_density_gcm3(pressure_bar):
+    """
+    Lookup table of NIST WebBook's density_gcm3 listing for xenon at various
     pressures.
 
     Input:
       pressure_bar
 
     Output:
-      density_gcm3
+      density_gcm3_gcm3
     """
 
-    density_table = {
+    density_gcm3_table = {
         '0.5': 0.0013950,
         '1': 0.0027907,
         '1.5': 0.0041873,
@@ -227,25 +395,25 @@ def NIST_webbook_Xe_density(pressure_bar):
         '30': 0.085029,
     }
 
-    if str(pressure_bar) in density_table:
-        return density_table[str(pressure_bar)]
+    if str(pressure_bar) in density_gcm3_table:
+        return density_gcm3_table[str(pressure_bar)]
     else:
         return np.nan
 
 
-def extrapolated_range_xe(E_kev, p_bar, ref="2002"):
-    E_kev = float(E_kev)
-    density = NIST_webbook_Xe_density(p_bar)
+def extrapolated_range_xe(energy_keV, p_bar, ref="2002"):
+    energy_keV = float(energy_keV)
+    density_gcm3 = NIST_webbook_Xe_density_gcm3(p_bar)
     Z = 54
     A = 131.29
-    I_EV = 482  # NIST ESTAR
-    return extrapolated_range(E_kev, Z, A, I_EV, density, ref)
+    I_eV = 482  # NIST ESTAR
+    return extrapolated_range(energy_keV, Z, A, I_eV, density_gcm3, ref)
 
 
-def extrapolated_range_136xe(E_kev, p_bar, ref="2002"):
-    E_kev = float(E_kev)
-    density = NIST_webbook_Xe_density(p_bar)
+def extrapolated_range_136xe(energy_keV, p_bar, ref="2002"):
+    energy_keV = float(energy_keV)
+    density_gcm3 = NIST_webbook_Xe_density_gcm3(p_bar)
     Z = 54
     A = 136     # enriched xenon assumed from NEXT simulations
-    I_EV = 482  # NIST ESTAR
-    return extrapolated_range(E_kev, Z, A, I_EV, density, ref)
+    I_eV = 482  # NIST ESTAR
+    return extrapolated_range(energy_keV, Z, A, I_eV, density_gcm3, ref)
