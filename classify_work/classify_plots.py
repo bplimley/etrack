bebfd12567f18d41b165717a766a7a75cd2e4995
Brian Plimley <as.white.as.snow@gmail.com>
2017-05-22 14:50:45 -0700

    classify_plots.plot_early_scatter and tweaks

--- classify_work/classify_plots.py
+++ classify_work/classify_plots.py
@@ -10,13 +10,17 @@ import matplotlib.pyplot as plt
 from make_bins import hardcoded_bins as get_bins
 import tally_results as tr
 
+MKR = ('*', 's', 'o', 'x', '^')
+MS = (8, 5, 5, 7, 6)
+
 
 def main():
     energy_bin_edges, beta_bin_edges = get_bins()
 
     datadict = get_data_dict()
 
-    plot_escape_fraction(datadict, energy_bin_edges, beta_bin_edges)
+    # plot_escape_fraction(datadict, energy_bin_edges, beta_bin_edges)
+    plot_early_scatter(datadict, energy_bin_edges, beta_bin_edges)
 
 
 def get_data_dict():
@@ -26,10 +30,12 @@ def get_data_dict():
     return datadict
 
 
-def plot_escape_fraction(datadict, e_bins, b_bins, abs_beta=True):
+def plot_escape_fraction(datadict, e_bins, b_bins, abs_beta=True, yerr=False):
     """
     Escape fraction vs. energy
-    Data series are different beta (or |beta|?)
+
+    abs_beta: if True, combine pos and neg beta bins
+    yerr: if False, assume y error bars are smaller than the markers, and skip
     """
 
     plt.figure(figsize=(8, 6))
@@ -63,7 +69,11 @@ def plot_escape_fraction(datadict, e_bins, b_bins, abs_beta=True):
             e_err[0, i] = e_mean[-1] - e_bins[i]
             e_err[1, i] = e_bins[i + 1] - e_mean[-1]
 
-        plt.errorbar(e_mean, f, yerr=f_unc, xerr=e_err, fmt='*',
+        m = MKR[j % len(MKR)]
+        ms = MS[j % len(MS)]
+        if not yerr:
+            f_unc = None
+        plt.errorbar(e_mean, f, yerr=f_unc, xerr=e_err, fmt=m, ms=ms,
                      label=u'{:.1f}° < β < {:.1f}°'.format(
                          b_bins[j], b_bins[j + 1]))
 
@@ -71,6 +81,64 @@ def plot_escape_fraction(datadict, e_bins, b_bins, abs_beta=True):
     plt.ylabel(u'Fraction escaping from 650 µm Si')
     plt.xlim((0, 500))
     plt.ylim((0, 0.6))
+    plt.grid('on')
+    plt.legend()
+    plt.show()
+
+
+def plot_early_scatter(datadict, e_bins, b_bins, abs_beta=True, yerr=False):
+    """
+    Early scatter vs. energy
+
+    abs_beta: if True, combine pos and neg beta bins
+    yerr: if False, assume y error bars are smaller than the markers, and skip
+    """
+
+    plt.figure(figsize=(8, 6))
+
+    n_bins = len(b_bins) - 1
+    if abs_beta:
+        jrange = range(n_bins / 2, n_bins)
+    else:
+        jrange = range(n_bins)
+
+    for j in jrange:
+        if abs_beta:
+            beta_lg = ((np.abs(datadict['beta_true_deg']) > b_bins[j]) &
+                       (np.abs(datadict['beta_true_deg']) <= b_bins[j + 1]))
+        else:
+            beta_lg = ((datadict['beta_true_deg'] > b_bins[j]) &
+                       (datadict['beta_true_deg'] <= b_bins[j + 1]))
+        f = []
+        f_unc = []
+        e_mean = []
+        e_err = np.zeros((2, len(e_bins) - 1))
+        for i in range(len(e_bins) - 1):
+            e_lg = ((datadict['energy_tot_kev'] > e_bins[i]) &
+                    (datadict['energy_tot_kev'] <= e_bins[i + 1]))
+            n_sc = np.sum(
+                beta_lg & e_lg & (datadict['early_scatter_flag'] == 1))
+            n_no = np.sum(
+                beta_lg & e_lg & (datadict['early_scatter_flag'] == 0))
+            n_tot = n_sc + n_no
+            f.append(float(n_sc) / float(n_tot))
+            f_unc.append(np.sqrt(n_tot * f[-1] * (1 - f[-1])) / float(n_tot))
+            e_mean.append(np.mean(datadict['energy_tot_kev'][e_lg & beta_lg]))
+            e_err[0, i] = e_mean[-1] - e_bins[i]
+            e_err[1, i] = e_bins[i + 1] - e_mean[-1]
+
+        m = MKR[j % len(MKR)]
+        ms = MS[j % len(MS)]
+        if not yerr:
+            f_unc = None
+        plt.errorbar(e_mean, f, yerr=f_unc, xerr=e_err, fmt=m, ms=ms,
+                     label=u'{:.1f}° < β < {:.1f}°'.format(
+                         b_bins[j], b_bins[j + 1]))
+
+    plt.xlabel('Energy [keV]')
+    plt.ylabel(u'Fraction scattering >30° in <25µm 2D')
+    plt.xlim((0, 500))
+    plt.ylim((0, 1))
     plt.grid('on')
     plt.legend()
     plt.show()
