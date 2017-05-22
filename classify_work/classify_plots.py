50ccac5bc898a7f59d7963b5ce04fff5e9e8c473
Brian Plimley <as.white.as.snow@gmail.com>
2017-05-22 15:04:01 -0700

    combine paper_plots.py and classify_plots.py

--- classify_work/classify_plots.py
+++ classify_work/classify_plots.py
@@ -13,6 +13,17 @@ import tally_results as tr
 MKR = ('*', 's', 'o', 'x', '^')
 MS = (8, 5, 5, 7, 6)
 
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=LARGE_SIZE)  # fontsize of the figure title
+plt.rc('text', usetex=False)
+
+arrowprops = {'width': 0, 'headwidth': 6, 'fc': 'k'}
+
 
 def main():
     energy_bin_edges, beta_bin_edges = get_bins()
@@ -20,7 +31,8 @@ def main():
     datadict = get_data_dict()
 
     # plot_escape_fraction(datadict, energy_bin_edges, beta_bin_edges)
-    plot_early_scatter(datadict, energy_bin_edges, beta_bin_edges)
+    # plot_early_scatter(datadict, energy_bin_edges, beta_bin_edges)
+    plot_escape_roc(datadict, energy_bin_edges, beta_bin_edges)
 
 
 def get_data_dict():
@@ -142,6 +154,78 @@ def plot_early_scatter(datadict, e_bins, b_bins, abs_beta=True, yerr=False):
     plt.grid('on')
     plt.legend()
     plt.show()
+
+
+def plot_escape_roc(datadict, e_bins, b_bins, abs_beta=True):
+    """Plot ROC for rejecting escape electrons."""
+
+    # based on tally_results.roc_curves()
+    test_thresholds_esc = np.arange(20, 101)
+    conf_list_esc = []
+    print('Building escape ROC curve with {} points'.format(
+        len(test_thresholds_esc)), end='')
+    for thresh in test_thresholds_esc:
+        caselist = tr.sort_cases(datadict, max_end_min_kev=thresh)
+        this_conf = tr.ConfusionMatrix.from_cases(
+            caselist, tr.ESCAPE_CASE_DICT, name='escape', thresh=thresh)
+        conf_list_esc.append(this_conf)
+        print('.', end='')
+    roc_esc = tr.RocCurve.from_confmat_list(conf_list_esc)
+
+    roc_esc.plot()
+
+
+def get_roc_data():
+    return tr.roc_curves()
+
+
+def low_end_roc_curve(roc_data):
+    """Plot the ROC curve for the low-energy end."""
+
+    roc, conf_list, test_thresholds = roc_data[0], roc_data[2], roc_data[4]
+    fig = make_roc_plot(roc, conf_list, test_thresholds, label='low')
+    plt.savefig('roc_low.png')
+
+
+def escape_roc_curve(roc_data):
+    """Plot the ROC curve for the escape."""
+
+    roc, conf_list, test_thresholds = roc_data[1], roc_data[3], roc_data[5]
+    fig = make_roc_plot(roc, conf_list, test_thresholds, label='escape')
+    plt.savefig('roc_esc.png')
+
+
+def make_roc_plot(roc, conf_list, test_thresholds, label=None):
+    """Plot the ROC curve for the low-energy end."""
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = roc.plot(color='C0', lw=2, label=label)
+    ax.grid('on')
+    # plt.plot([0, 1], [0, 1], ':k', lw=2)
+
+    if len(test_thresholds) > 50:
+        interval = 10
+    else:
+        interval = 5
+    label_indices = np.arange(0, len(test_thresholds), interval).astype(int)
+
+    for ind in label_indices:
+        xydata = (roc.FPR[ind], roc.TPR[ind])
+        xytext = (xydata[0] + 0.12, xydata[1] - 0.12)
+        ax.annotate('{} keV'.format(test_thresholds[ind]),
+                    xy=xydata, xytext=xytext, arrowprops=arrowprops,
+                    size=14)
+
+    # ind = np.flatnonzero(
+    #     test_thresholds == tr.DEFAULT_MIN_END_MAX_KEV)[0]
+    # confmat = conf_list[ind]
+    # plt.plot(confmat.FPR, confmat.TPR, '*g', ms=12, lw=3)
+
+    plt.xlabel('False Positive Rate (track discarded erroneously)',
+               fontsize=15)
+    plt.ylabel('True Positive Rate (track discarded correctly)', fontsize=15)
+
+    return fig
 
 
 if __name__ == '__main__':
